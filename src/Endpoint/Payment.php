26adcd00c63b77318552cee0f40f3de656160055
Muhannad <w7sh.oman@gmail.com>
2020-12-03 04:30:51 +0400

    Add payment endpoints callback

---
+++ src/Endpoint/Payment.php
@@ -0,0 +1,56 @@
+<?php
+namespace Thawani\Endpoint;
+
+/**
+ * Thwawni gateWay Payment endpoint handler 
+ *
+ * Integration of Thawani API class 
+ *
+ * @class       Payment
+ * @version     1.0.0
+ * @author      Muhannad Alrisi
+ * @package     WooCommerce\Thawani
+ */
+class Payment
+{
+
+    private $api = null;
+
+    public function __construct(RestAPI $api)
+    {
+        $this->api = $api;
+    }
+
+    /**
+     * This endpoint will return the card token that has been stored against a specific customer
+     * using the customer token which was created previously. Use this method to retrieve the card
+     * token and include it in the “create session” endpoint to process payment.
+     *
+     * @param string $customer_token
+     *
+     * @return array response
+     */
+    public function get($customer_token)
+    {
+        return wp_remote_get($this->api->get_endpoint('api/v1/payment_methods?customerId=') . $customer_token,
+            [
+                'headers' => $this->gapi->get_headers(),
+            ]);
+    }
+
+    /**
+     * This endpoint is used to remove specific payment method for the customer.
+     *
+     * @param string $card_token
+     *
+     * @return array response
+     */
+    public function delete($card_token)
+    {
+        return wp_remote_request($this->api->get_endpoint('api/v1/payment_methods/') . $customer_token,
+            [
+                'method' => 'DELETE',,
+            ]);
+    }
+
+}
--- src/RestAPI.php
+++ src/RestAPI.php
@@ -3,12 +3,13 @@
 namespace Thawani;
 
 use Thawani\Endpoint\Customer;
+use Thawani\Endpoint\Payment;
 use Thawani\Endpoint\Session;
 
 /**
- * Thwawni REST API Handler 
+ * Thwawni REST API Handler
  *
- *  the skeleton of the REST API 
+ *  the skeleton of the REST API
  *
  * @class       RestAPI
  * @version     1.0.0
@@ -27,44 +28,46 @@ class RestAPI
 
     private $session = null;
     private $customer = null;
+    private $payment = null;
     public function __construct($secret_key, $publishable_key, $env)
     {
         $this->secret_key = $secret_key;
         $this->publishable_key = $publishable_key;
-        $this->env  = $env;
+        $this->env = $env;
 
         $this->session = new Session($this);
         $this->customer = new Customer($this);
+        $this->payment = new Payment($this);
     }
 
     /**
      * Get the API endpoint depend on the environmen
-     * 
-     * @return string endpoint  
+     *
+     * @return string endpoint
      */
     public function get_endpoint_env()
     {
         return (strtolower($this->env) == 'development')
-            ? self::DEV_ENDPOINT
-            : self::PROD_ENDPOINT;
+        ? self::DEV_ENDPOINT
+        : self::PROD_ENDPOINT;
     }
 
     /**
-     * prepare HTTP header token in the request 
-     * 
-     * @return array HTTP header 
+     * prepare HTTP header token in the request
+     *
+     * @return array HTTP header
      */
     public function get_headers()
     {
         return [
             'Content-Type' => 'Application/json',
-            'Thawani-Api-Key' => $this->secret_key
+            'Thawani-Api-Key' => $this->secret_key,
         ];
     }
     /**
      * get the publishable key
-     * 
-     * @return string publishable_key 
+     *
+     * @return string publishable_key
      */
     public function get_publishable_key()
     {
@@ -73,9 +76,9 @@ class RestAPI
     /**
      * prepare the endpoint for the remote request HTTP client
      * Do not add / in the suffix e.g api/get/detail not /api/get/detail
-     * 
+     *
      * @param string $suffix e.g api/get/detail not /api/get/detail
-     * 
+     *
      * @return string full endpoint url
      */
     public function get_endpoint($suffix)
@@ -84,36 +87,36 @@ class RestAPI
     }
 
     /**
-     * Create Thawani session 
-     * 
+     * Create Thawani session
+     *
      * @param array $payload request body
-     * 
-     * @return array response 
+     *
+     * @return array response
      */
     public function create_session($payload)
     {
-        return  $this->session->create($payload);
+        return $this->session->create($payload);
     }
     /**
-     * Get  full redirect path to thawani payment 
-     * 
+     * Get  full redirect path to thawani payment
+     *
      * @param string $sesion_key
-     * 
-     * @return string uri path 
+     *
+     * @return string uri path
      */
     public function get_redirect_uri($sesion_key)
     {
         return $this->session->redirect($sesion_key);
     }
     /**
-     * create customer token 
-     * 
-     * @return string\boolean if success it returns customer token otherwise false 
+     * create customer token
+     *
+     * @return string\boolean if success it returns customer token otherwise false
      */
     public function create_customer()
     {
-        $response  = $this->customer->create();
-        $data  = json_decode($response['body']);
+        $response = $this->customer->create();
+        $data = json_decode($response['body']);
         if (isset($data->success) && $data->success) {
             $this->customer->set_customer_meta($data->data->id);
             return $data->data->id;
@@ -122,25 +125,50 @@ class RestAPI
     }
     /**
      * Get the customer token or id
-     * 
+     *
      * @return string customer token or id
      */
     public function get_customer()
     {
-        if ($this->customer->get_customer_meta() == '')
+        if ($this->customer->get_customer_meta() == '') {
             return $this->create_customer();
+        }
 
         return $this->customer->get_customer_meta();
     }
     /**
      * get the session from Thawani gateway
-     * 
-     * @param string $session_token 
-     * 
+     *
+     * @param string $session_token
+     *
      * @return array response
      */
     public function get_session($session_token)
     {
         return $this->session->get($session_token);
+    }
+
+    /**
+     * get payment method of the customers
+     *
+     * @param string $customer_token
+     *
+     * @return array response
+     */
+    public function get_payment($cutomer_token)
+    {
+        return $this->payment->get($cutomer_token);
+    }
+
+    /**
+     *  remove specific payment method for the customer.
+     *
+     * @param string $card_token
+     *
+     * @return array response
+     */
+    public function delete_payment_method($card_token)
+    {
+        return $this->payment->delete($card_token);
     }
 }
