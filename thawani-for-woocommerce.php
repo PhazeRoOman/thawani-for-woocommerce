737bc9b3f41d6a241e921de8520fe8e5de6d99b4
Maher Alkendi <maheralkendi@Mahers-MacBook-Air.local>
2021-07-23 01:16:52 +0400

    Fix: plugin uri and license updated

--- thawani-for-woocommerce.php
+++ thawani-for-woocommerce.php
@@ -2,12 +2,12 @@
 
 /**
  * Plugin Name: Thawani for WooCommerce
- * Plugin URI: https://phaze.ro/
+ * Plugin URI: https://github.com/PhazeRoOman/thawani-for-woocommerce
  * Author: PhazeRo
  * Author URI: https://phaze.ro/
  * Description: Thawani Payments Gateway for Woocommerce.
  * Version: 1.2.0
- * License: GPL2
+ * License: MIT
  * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
  * text-domain: thawani
  * Domain Path : /languages
